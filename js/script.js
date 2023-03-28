fb14753975efb9ed955c468745bb79fa2247fa06
Felipe H Souza <75103710+SouzaHFelipe@users.noreply.github.com>
2023-03-27 22:08:21 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,7 +4,10 @@ const todoInput = document.querySelector("#todo-input");
 const todoList = document.querySelector("#todo-list");
 const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
-const cancelEditBtn = document.querySelector("#cancel-edit-brn");
+const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+
+let oldInputValue;
+
 
 //funcoes
  const saveTodo = (text) => {
@@ -40,8 +43,28 @@ const cancelEditBtn = document.querySelector("#cancel-edit-brn");
 
  }
 
+ const toggleForms = () =>{
 
-//eventops
+    editForm.classList.toggle("hide")
+    todoForm.classList.toggle("hide")
+    todoList.classList.toggle("hide")
+ }
+
+
+ const updateTodo = (text) => {
+        const todos = document.querySelectorAll(".todo");
+
+        todos.forEach((todo) => {
+            let todoTitle = todo.querySelector("h3");
+
+            if(todoTitle.innerText === oldInputValue ) {
+                todoTitle.innerText = text;
+
+            }
+        })
+ }
+
+//eventos
 
 todoForm.addEventListener("submit" , (e) => {
     e.preventDefault()
@@ -54,10 +77,17 @@ todoForm.addEventListener("submit" , (e) => {
     }
 });
 
-document.addEventListener("click" , (e) =>{
+document.addEventListener("click", (e) =>{
 
     const targetEl = e.target;
     const parentEl = targetEl.closest("div");
+    let todoTitle;
+
+    if(parentEl && parentEl.querySelector("h3")){
+        todoTitle = parentEl.classList.toggle("done").innerText;
+
+
+    }
 
 if(targetEl.classList.contains("finish-todo")) {
     parentEl.classList.toggle("done");
@@ -71,8 +101,33 @@ if(targetEl.classList.contains("remove-todo")){
 }
 
 if(targetEl.classList.contains("edit-todo")){
+    toggleForms();
     
+    editInput.value = todoTitle;
+    oldInputValue = todoTitle;
 }
+
+
+});
+
+cancelEditBtn.addEventListener("click", (e) =>{
+    e.preventDefault();
+
+    toggleForms();
+
+} )
+
+editForm.addEventListener("submit", (e) => {
+
+    e.preventDefault()
+
+    const editInputValue = editInput.value
+
+    if(editInputValue){
+        updateTodo(editInputValue)
+    }
+
+    toggleForms()
 
 
 })
